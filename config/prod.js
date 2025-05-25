57e6e903cbdd2ec19f8fd7a2b6c5007bf2bea294
danielw4335 <daniel.wallache@gmail.com>
2025-05-25 13:20:43 +0300

    build

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,4 @@
 export default {
-  dbURL: process.env.MONGO_URL || 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
+  dbURL: process.env.MONGO_URL || 'mongodb+srv://danielWallache:6543355@cluster0.k53mbev.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
   dbName : process.env.DB_NAME || 'story_db'
 }
